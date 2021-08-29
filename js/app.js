d45f9a9faf15e925ea53cc802302f0959cf1e23a
Ibrahim H <mohammad.ihakash@gmail.com>
2021-08-29 08:51:34 +0600

    complete

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const getMealName = () => {
+    const mealName = document.getElementById('search-field');
+    loadData(mealName);
+    mealName.value = '';
+}
+
+const loadData = async mealID => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealID.value}`;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    console.log(jsonData.meals[0]);
+    processData(jsonData.meals)
+}
+
+const processData = meals => {
+    const postContainer = document.getElementById('post-container');
+    meals.forEach(meal => {
+        console.log(meal);
+        const newPost = document.createElement('div');
+        newPost.classList.add('col-12', 'col-lg-4', 'col-md-6', 'text-center');
+        newPost.innerHTML = `
+        <img class="meal-img" src="${meal.strMealThumb}">
+        <h5>${meal.strMeal}</h5>
+        `;
+        postContainer.appendChild(newPost);
+    })
+}
